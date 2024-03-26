0b1e8a41cb4ae22ea631a8b11cbc1b4048883535
karlos <karloszuru@gmail.com>
2024-03-26 13:50:01 -0500

    comments in axios instance.

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'https://your.api/base/url',
+    // baseURL: 'https://your.api/base/url', // where we end up hosting the server
     baseURL: 'https://http://127.0.0.1:8000/', // for local development with django running
 });
 
