81a6a16ece31bda167882fb401bca637396f4d29
“Gemma <122994950+gemmagoddard3@users.noreply.github.com.>
2024-03-29 22:18:00 -0500

    changing base url for rotating images to work any host

--- frontend/src/components/RotatingImage.js
+++ frontend/src/components/RotatingImage.js
@@ -3,7 +3,6 @@ import AspectRatio from '@mui/joy/AspectRatio';
 
 function RotatingImage({ imageList }) {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const baseURL = 'http://127.0.0.1:8000';
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -17,7 +16,7 @@ function RotatingImage({ imageList }) {
         <AspectRatio variant="outlined" ratio="9/17" objectFit="cover">
         {/* only layout="fill" makes sense for using with AspectRatio */}
         <img
-            src={baseURL + imageList[currentIndex]}
+            src={imageList[currentIndex]}
             alt="Test image"
             style={{width:'100%', height: '100%', borderRadius: '5px', backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}} 
             
