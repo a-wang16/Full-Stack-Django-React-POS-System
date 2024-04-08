dc95bac17cc8f095f5a9849c12b7079d95399712
“Gemma <122994950+gemmagoddard3@users.noreply.github.com.>
2024-04-08 11:11:38 -0500

    Resizing manager graph

--- frontend/src/pages/GraphExamplePage.js
+++ frontend/src/pages/GraphExamplePage.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axiosInstance from '../utils/axiosInstance';
-import {Box, Button, Grid, Input, Sheet, Stack, Typography} from "@mui/joy";
+import { Box, Button, Grid, Input, Sheet, Stack, Typography } from "@mui/joy";
 import MenuItemCard from "../components/MenuItemCard";
 import { useOrder } from "../utils/OrderContext";
 import { useNavigate } from "react-router-dom";
 import CircularProgress from "@mui/joy/CircularProgress";
-import {Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
+import { Line, LineChart, Tooltip, XAxis, YAxis, ResponsiveContainer } from "recharts";
 import moment from "moment";
 
 function GraphExamplePage() {
@@ -24,7 +24,7 @@ function GraphExamplePage() {
 
     const fetchMenuItems = async () => {
         try {
-            const response = await axiosInstance.get('/api/orders-per-day/?start_date='+initialDate+'&end_date='+finalDate);
+            const response = await axiosInstance.get('/api/orders-per-day/?start_date=' + initialDate + '&end_date=' + finalDate);
             console.log(response.data);
             const formattedData = response.data.map(item => ({
                 ...item,
@@ -57,99 +57,95 @@ function GraphExamplePage() {
     return (
         <Box sx={{ height: '100%', width: '100%' }}>
 
-        <Stack direction={'row'} sx={{ height: '100%', width: '100%' }}>
-            <Sheet variant={'soft'} sx={{
-                width: '20vw',
-                maxWidth: '20vw',
-                minWidth: '20vw',
-                flexDirection: 'column',
-                justifyContent: 'flex-start',
-                alignItems: 'center',
-                height: '100vh',
-            }}>
+            <Stack direction={'row'} sx={{ height: '100%', width: '100%' }}>
+                <Sheet variant={'soft'} sx={{
+                    width: '20vw',
+                    maxWidth: '20vw',
+                    minWidth: '20vw',
+                    flexDirection: 'column',
+                    justifyContent: 'flex-start',
+                    alignItems: 'center',
+                    height: '100vh',
+                }}> </Sheet>
 
-
-            </Sheet>
-
-            <Stack margin={2}>
-                <Sheet variant={'plain'}
-                       color={'neutral'}
-                       sx={{
-                        width: '100%',
-                        height: '5vh',
-                        flexDirection: 'row',
-                }}
-                >
-                    <Stack
-                        direction={'row'}
-                        justifyContent="flex-end"
+                <Stack margin={6} spacing={2} sx={{ width: '100vw' } }>
+                    <Sheet variant={'plain'}
+                        color={'neutral'}
+                        sx={{
+                            width: '100%',
+                            height: '5vh',
+                            flexDirection: 'row',
+                            // backgroundColor: 'black'
+                        }}
                     >
                         <Typography level="h3" sx={{ margin: 1 }}>Graph View</Typography>
-                        {/*<Typography level="h3" sx={{ margin: 1 }}>Weather API</Typography>*/}
 
-                    </Stack>
-                </Sheet>
+                    </Sheet>
 
-                 <Input
-                    type="date"
-                    slotProps={{
-                      input: {
-                        min: '2018-06-07',
-                        max:  currDate,
-                      },
-                    }}
-                    id='initial'
-                    defaultValue={'2018-06-07'}
-                    onChange={function() {
-                        initialDate = document.getElementById("initial").value;
-                        fetchMenuItems();
-                    }}
-                  />
+                    <Input
+                        type="date"
+                        slotProps={{
+                            input: {
+                                min: '2018-06-07',
+                                max: currDate,
+                            },
+                        }}
+                        id='initial'
+                        defaultValue={'2018-06-07'}
+                        onChange={function () {
+                            initialDate = document.getElementById("initial").value;
+                            fetchMenuItems();
+                        }}
+                        sx={{ width: '200px' }}
+                    />
 
-                <Input
-                    type="date"
-                    slotProps={{
-                      input: {
-                        min: '2018-06-07',
-                        max:  currDate,
-                      },
-                    }}
-                    id='final'
-                    defaultValue={currDate}
-                    onChange={function() {
-                        finalDate = document.getElementById("final").value;
-                        fetchMenuItems();
-                    }}
-                  />
+                    <Input
+                        type="date"
+                        slotProps={{
+                            input: {
+                                min: '2018-06-07',
+                                max: currDate,
+                            },
+                        }}
+                        id='final'
+                        defaultValue={currDate}
+                        onChange={function () {
+                            finalDate = document.getElementById("final").value;
+                            fetchMenuItems();
+                        }}
+                        sx={{ width: '200px' }}
+                    />
 
-            {data.length == 0 && (
-                <Typography variant="h4" style={{ color: 'red', marginTop: '10px' }}>
-                    No data for given date range.
-                </Typography>
-            )}
+                    {data.length == 0 && (
+                        <Typography variant="h4" style={{ color: 'red', marginTop: '10px' }}>
+                            No data for given date range.
+                        </Typography>
+                    )}
 
-            {data.length != 0 && (
-                <LineChart width={600} height={300} data={data}>
-                    <XAxis dataKey="date"/>
-                    <YAxis />
-                    <Line type="monotone" dataKey="count" stroke="#8884d8" />
-                    <Tooltip />
-                </LineChart>
-            )}
-            
+                    {data.length != 0 && (
+                        <ResponsiveContainer width="90%"  height="80%">
+                            <LineChart width={900} height={900} data={data}>
+                                <XAxis dataKey="date" />
+                                <YAxis />
+                                <Line type="monotone" dataKey="count" stroke="#8884d8" />
+                                <Tooltip />
+                            </LineChart>
+                        </ResponsiveContainer>
+                    )}
+
+
+                </Stack>
 
             </Stack>
-
-        </Stack>
         </Box>
 
     );
-    
-    document.getElementById("initial").addEventListener("change", function() {
+
+    document.getElementById("initial").addEventListener("change", function () {
         initialDate = this.value;
     });
 
-    document.getElementById("final").addEventListener("change", function() {
+    document.getElementById("final").addEventListener("change", function () {
         finalDate = this.value;
     });
 }
